0181d4fd30a7c655769b8b132e406c47e63f75a4
404 Web Services <wwebservices404@gmail.com>
2026-05-06 01:30:45 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/smartwork-dashboard/', // <-- Add this exact line
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
-});
+})
